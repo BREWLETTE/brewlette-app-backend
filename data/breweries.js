4583b35c72193a7c56f2b89dd4997494d153bbd7
Cristian Montes <cmontesmujica@gmail.com>
2021-08-30 11:24:49 -0700

    user and brewery tables working

---
+++ data/breweries.js
@@ -0,0 +1,32 @@
+module.exports = [
+  {
+    brewery_id: 200,
+    name: 'Breckenridge BBQ and Brew Pub',
+    visited: false,
+    user_id: 1
+  },
+  {
+    brewery_id: 8352,
+    name: 'Vine Street Pub and Brewery',
+    visited: false,
+    user_id: 1
+  },
+  {
+    brewery_id: 12916,
+    name: 'Highland Tap and Burger',
+    visited: false,
+    user_id: 1
+  },
+  {
+    brewery_id: 13875,
+    name: 'Copper Kettle Brewing Company',
+    visited: false,
+    user_id: 1
+  },
+  {
+    brewery_id: 13995,
+    name: 'Freshcraft',
+    visited: false,
+    user_id: 1
+  }
+];
