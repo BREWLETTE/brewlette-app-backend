8b6e71d6d4720dd18eab1521fc18e44909142ad9
Cristian Montes <cmontesmujica@gmail.com>
2021-08-30 19:23:28 -0700

    working on api fetch utils

--- data/create-tables.js
+++ data/create-tables.js
@@ -14,7 +14,7 @@ async function run() {
     await client.query(`
                 CREATE TABLE users (
                     id SERIAL PRIMARY KEY,
-                    name VARCHAR(512) NOT NULL,
+                    name VARCHAR(512),
                     email VARCHAR(256) NOT NULL,
                     hash VARCHAR(512) NOT NULL
                 );           
